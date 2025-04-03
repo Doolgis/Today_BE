ccd07d1d03d919bf2e8783cdb0576a16eb92ff13
0lYUMA <102151689+0lYUMA@users.noreply.github.com>
2025-04-03 16:25:05 +0900

    feat: 개발환경 세팅

---
+++ src/main/java/com/example/today_be/TodayBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.today_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodayBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/today_be/TodayBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.today_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodayBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
