55898931cc6ebfda96de0ccf90e43e1ada5dca7f
0lYUMA <102151689+0lYUMA@users.noreply.github.com>
2025-04-03 17:39:26 +0900

    feat: Sample Data 요청

--- src/main/java/com/example/today_be/Bike/BikeController.java
+++ src/main/java/com/example/today_be/Bike/BikeController.java
@@ -1,4 +1,57 @@
 package com.example.today_be.Bike;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+@RestController
+@RequestMapping("/bike")
 public class BikeController {
+
+    @GetMapping("/test")
+    public String test22() {
+        return "testest";
+    }
+
+    @GetMapping("/test222")
+    public void test() throws IOException {
+        StringBuilder urlBuilder = new StringBuilder("http://openapi.seoul.go.kr:8088/6870584e4379756e3936566761644c");
+        urlBuilder.append("/" + URLEncoder.encode("xml", "UTF-8"));
+        urlBuilder.append("/" + URLEncoder.encode("citydata", "UTF-8"));
+        urlBuilder.append("/" + URLEncoder.encode("1", "UTF-8"));
+        urlBuilder.append("/" + URLEncoder.encode("5", "UTF-8"));
+        urlBuilder.append("/" + URLEncoder.encode("POI001", "UTF-8"));
+
+        URL url = new URL(urlBuilder.toString());
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Content-type", "application/xml");
+        System.out.println("Response Code : " + conn.getResponseCode());
+
+        BufferedReader br;
+
+        if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+            br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        } else {
+            br = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+        }
+
+        StringBuilder sb = new StringBuilder();
+        String line;
+
+        while ((line = br.readLine()) != null) {
+            sb.append(line);
+        }
+
+        br.close();
+        conn.disconnect();
+        System.out.println(sb.toString());
+    }
 }
