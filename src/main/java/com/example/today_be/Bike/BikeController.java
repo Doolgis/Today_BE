4b69153392ad88c19d486abb9563d2afb7a93cc8
0lYUMA <102151689+0lYUMA@users.noreply.github.com>
2025-04-09 20:24:43 +0900

    feat: 환경변수 처리 및 데이터 반환 JSON 형식으로 변환

--- src/main/java/com/example/today_be/Bike/BikeController.java
+++ src/main/java/com/example/today_be/Bike/BikeController.java
@@ -1,5 +1,6 @@
 package com.example.today_be.Bike;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,15 +16,13 @@ import java.net.URLEncoder;
 @RequestMapping("/bike")
 public class BikeController {
 
-    @GetMapping("/test")
-    public String test22() {
-        return "testest";
-    }
+    @Value("${DATA_KEY}")
+    private String dataKey;
 
-    @GetMapping("/test222")
+    @GetMapping("/test")
     public void test() throws IOException {
-        StringBuilder urlBuilder = new StringBuilder("http://openapi.seoul.go.kr:8088/6870584e4379756e3936566761644c");
-        urlBuilder.append("/" + URLEncoder.encode("xml", "UTF-8"));
+        StringBuilder urlBuilder = new StringBuilder("http://openapi.seoul.go.kr:8088/" + dataKey);
+        urlBuilder.append("/" + URLEncoder.encode("json", "UTF-8"));
         urlBuilder.append("/" + URLEncoder.encode("citydata", "UTF-8"));
         urlBuilder.append("/" + URLEncoder.encode("1", "UTF-8"));
         urlBuilder.append("/" + URLEncoder.encode("5", "UTF-8"));
@@ -32,7 +31,7 @@ public class BikeController {
         URL url = new URL(urlBuilder.toString());
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
-        conn.setRequestProperty("Content-type", "application/xml");
+        conn.setRequestProperty("Content-type", "application/json");
         System.out.println("Response Code : " + conn.getResponseCode());
 
         BufferedReader br;
@@ -52,6 +51,6 @@ public class BikeController {
 
         br.close();
         conn.disconnect();
-        System.out.println(sb.toString());
+        System.out.println(sb);
     }
 }
