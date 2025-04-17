575d7c087ee8ed9f3525ae07d0b9a7fdde36ab7a
0lYUMA <102151689+0lYUMA@users.noreply.github.com>
2025-04-17 18:31:21 +0900

    feat: 날씨에 따른 따릉이 추천 여부, 5km 내 따릉이 코스 추천 기능 추가

--- src/main/java/com/example/today_be/Bike/BikeController.java
+++ src/main/java/com/example/today_be/Bike/BikeController.java
@@ -1,6 +1,8 @@
 package com.example.today_be.Bike;
 
 import com.example.today_be.Bike.dto.BikeResponseDto;
+import com.example.today_be.Bike.dto.RecommendedCourseDto;
+import com.example.today_be.Weather.dto.WeatherResponseDto;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -16,6 +18,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -58,7 +61,7 @@ public class BikeController {
 
         br.close();
         conn.disconnect();
-        System.out.println(sb);
+//        System.out.println(sb);
 
         ObjectMapper objectMapper = new ObjectMapper();
         JsonNode root = objectMapper.readTree(sb.toString());
@@ -92,5 +95,133 @@ public class BikeController {
         double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
         return R * c;
+    }
+
+    @GetMapping("/weather")
+    public void resultBike() throws IOException {
+        StringBuilder urlBuilder = new StringBuilder("http://openapi.seoul.go.kr:8088/" + dataKey);
+        urlBuilder.append("/" + URLEncoder.encode("json", "UTF-8"));
+        urlBuilder.append("/" + URLEncoder.encode("citydata", "UTF-8"));
+        urlBuilder.append("/" + URLEncoder.encode("1", "UTF-8"));
+        urlBuilder.append("/" + URLEncoder.encode("10", "UTF-8"));
+        urlBuilder.append("/" + URLEncoder.encode("강남역", "UTF-8"));
+
+        URL url = new URL(urlBuilder.toString());
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Content-type", "application/json");
+        System.out.println("Response Code : " + conn.getResponseCode());
+
+        BufferedReader br;
+
+        if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+            br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        } else {
+            br = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+        }
+
+        StringBuilder sb = new StringBuilder();
+        String line;
+
+        while ((line = br.readLine()) != null) {
+            sb.append(line);
+        }
+
+        br.close();
+        conn.disconnect();
+//        System.out.println(sb);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode root = objectMapper.readTree(sb.toString());
+
+        JsonNode resultBike = root.at("/CITYDATA/WEATHER_STTS");
+
+        JsonNode weather = resultBike.get(0);
+
+        WeatherResponseDto weatherResponseDto = objectMapper.readValue(weather.toString(), new TypeReference<WeatherResponseDto>() {});
+        System.out.println(weatherResponseDto);
+        System.out.println(isSuitableForBike(weatherResponseDto));
+    }
+
+    // 오늘 날씨가 따릉이를 탈만한 날씨인지에 대한 판단 여부
+    public boolean isSuitableForBike(WeatherResponseDto weather) {
+        try {
+            double temp = Double.parseDouble(weather.getTemp());
+            double windSpd = Double.parseDouble(weather.getWindSpd());
+            String precptType = weather.getPrecptType();
+            String pm10Index = weather.getPm10Index(); // "좋음", "보통", "나쁨" 등
+
+            boolean tempOk = temp >= 0 && temp <= 30;
+            boolean windOk = windSpd <= 5.0;
+            boolean rainOk = "없음".equals(precptType);
+            boolean airOk = "좋음".equals(pm10Index) || "보통".equals(pm10Index);
+
+            return tempOk && windOk && rainOk && airOk;
+
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    @GetMapping("/recommand-course")
+    public List<RecommendedCourseDto> recommandCourse(@RequestParam double userX, @RequestParam double userY) throws IOException {
+        StringBuilder urlBuilder = new StringBuilder("http://openapi.seoul.go.kr:8088/" + dataKey);
+        urlBuilder.append("/" + URLEncoder.encode("json", "UTF-8"));
+        urlBuilder.append("/" + URLEncoder.encode("bikeList", "UTF-8"));
+        urlBuilder.append("/" + URLEncoder.encode("1", "UTF-8"));
+        urlBuilder.append("/" + URLEncoder.encode("1000", "UTF-8"));
+
+        URL url = new URL(urlBuilder.toString());
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Content-type", "application/json");
+        System.out.println("Response Code : " + conn.getResponseCode());
+
+        BufferedReader br;
+
+        if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+            br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        } else {
+            br = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+        }
+
+        StringBuilder sb = new StringBuilder();
+        String line;
+
+        while ((line = br.readLine()) != null) {
+            sb.append(line);
+        }
+
+        br.close();
+        conn.disconnect();
+//        System.out.println(sb);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode root = objectMapper.readTree(sb.toString());
+        JsonNode bikeArray = root.path("rentBikeStatus").path("row");
+
+        List<RecommendedCourseDto> candidates = new ArrayList<>();
+
+        for (JsonNode station : bikeArray) {
+            double lat = station.path("stationLatitude").asDouble();  // 위도
+            double lng = station.path("stationLongitude").asDouble(); // 경도
+            int parkingCount = station.path("parkingBikeTotCnt").asInt(); // 주차 가능 따릉이 수
+
+            double distance = calculateDistance(userY, userX, lat, lng);
+            System.out.println("distance : " + distance);
+
+            if (distance >= 4.5 && distance <= 5.5 && parkingCount > 0) {
+                candidates.add(RecommendedCourseDto.builder()
+                        .startLat(userY)
+                        .startLon(userX)
+                        .endLat(lat)
+                        .endLon(lng)
+                        .endStationName(station.get("stationName").asText())
+                        .distanceKm(distance)
+                        .build());
+            }
+        }
+
+        return candidates;
     }
 }
